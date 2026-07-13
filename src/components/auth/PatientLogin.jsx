8ba317ba012487eeb353b8be637482d0cf938ccd
Siddhartha Vuggi <141249647+SiddharthaVuggi@users.noreply.github.com>
2026-07-13 20:44:04 +0530

    Refactor PatientLogin component for consistency

--- src/components/auth/PatientLogin.jsx
+++ src/components/auth/PatientLogin.jsx
@@ -1,20 +1,21 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import '../../styles/Signup.css';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../../styles/Signup.css";
 
 const PatientLogin = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const navigate = useNavigate();
 
   const handleLogin = (e) => {
     e.preventDefault();
 
-    if (email === 'patient@gmail.com' && password === 'patient123') {
-      alert('Login successful');
-      navigate('/patient/dashboard');
+    if (email === "patient@gmail.com" && password === "patient123") {
+      alert("Login successful");
+      navigate("/patient/dashboard");
     } else {
-      alert('Invalid Patient credentials');
+      alert("Invalid Patient credentials");
     }
   };
 
@@ -22,8 +23,23 @@ const PatientLogin = () => {
     <div className="video-bg-wrapper patient-bg">
       <form onSubmit={handleLogin} className="signup-form">
         <h2 className="pat-log">Patient Login</h2>
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Patient Email"required />
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
+
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Patient Email"
+          required
+        />
+
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          required
+        />
+
         <button type="submit">Login</button>
       </form>
     </div>
