ee8f7bb6c07c3623a5a1600d4234f1ca56ba9755
Siddhartha Vuggi <141249647+SiddharthaVuggi@users.noreply.github.com>
2026-07-14 20:39:29 +0530

    Refactor DoctorDashboard JSX formatting and imports

--- src/pages/DoctorDashboard.jsx
+++ src/pages/DoctorDashboard.jsx
@@ -1,27 +1,36 @@
-import React from 'react';
-import PatientTable from '../components/doctor/PatientTable';
-import IncidentTable from '../components/doctor/IncidentTable';
-import '../styles/DoctorDashboard.css';
-import AppointmentCalendar from '../components/calendar/AppointmentCalendar';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
+import AppointmentCalendar from "../components/calendar/AppointmentCalendar";
+import IncidentTable from "../components/doctor/IncidentTable";
+import PatientTable from "../components/doctor/PatientTable";
+
+import "../styles/DoctorDashboard.css";
 
 const DoctorDashboard = () => {
   const navigate = useNavigate();
+
   return (
-    <>
-    <div className='dash'>
+    <div className="dash">
       <div className="header-container">
         <h1 className="hello">Hello, Dentist</h1>
-          <button className="home-button" onClick={() => navigate('/')}>Logout</button>
+
+        <button
+          className="home-button"
+          onClick={() => navigate("/")}
+        >
+          Logout
+        </button>
       </div>
+
       <AppointmentCalendar />
-      <div style={{ padding : '2rem' }}>
-      <PatientTable /><br/>
-      <IncidentTable />
+
+      <div style={{ padding: "2rem" }}>
+        <PatientTable />
+        <br />
+        <IncidentTable />
       </div>
     </div>
-    </>
   );
 };
 
